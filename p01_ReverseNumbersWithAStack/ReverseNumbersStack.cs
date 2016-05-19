3c0d6dd6f6509904a79ffb9b3deca76a668017dc
Stoyan Stoyanov <stoyan_ms@yahoo.com>
2016-05-19 11:44:10 +0300

    ready_01and02

---
+++ p01_ReverseNumbersWithAStack/ReverseNumbersStack.cs
@@ -0,0 +1,36 @@
+﻿
+using System;
+using System.Collections.Generic;
+// using System.Text.RegularExpressions;
+
+namespace p01_ReverseNumbersWithAStack
+{
+    public class ReverseNumbersStack
+    {
+        public static void Main()
+        {
+            // begin
+            Stack<int> numbers = new Stack<int>();
+
+            string inputLine = Console.ReadLine();
+            // string newStrstr = Regex.Replace(inputLine, " {2,}", " ");
+            string[] inputLineStr = inputLine.Trim().Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+
+            // pushing the elements
+            for (int i = 0; i < inputLineStr.Length; i++)
+            {
+                numbers.Push(Convert.ToInt32(inputLineStr[i]));
+            }
+
+            while (numbers.Count != 0)
+            {
+                Console.Write(numbers.Pop());
+                if (numbers.Count > 0)
+                {
+                    Console.Write(" ");
+                }
+            }
+            Console.WriteLine();
+        }
+    }
+}
---
+++ p02_BasicStackOperations/BasicStackOparations.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace p02_BasicStackOperations
+{
+    public class BasicStackOparations
+    {
+        public static void Main()
+        {
+            // begin
+            Stack<int> intNumbersStack = new Stack<int>();
+            // reading data from console
+            int[] numbers = Console.ReadLine()
+                .Trim()
+                .Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+            int amountOfElementsToPush = numbers[0];     // amount of elements in the stack
+            int amountOfElementsToPop = numbers[1];      // elements to pop
+            int elementToCheck = numbers[2];             // check if exist
+
+            // input elements and push them to the stack
+            // reading from console
+            numbers = Console.ReadLine()
+                .Trim()
+                .Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+
+            //pushing elements
+            for (int i = 0; i < amountOfElementsToPush; i++)
+            {
+                intNumbersStack.Push(numbers[i]);
+            }
+
+            // pop elements
+            for (int i = 0; i < amountOfElementsToPop; i++)
+            {
+                intNumbersStack.Pop();
+            }
+
+            // printing the result
+            if (intNumbersStack.Count == 0)
+            {
+                Console.WriteLine("0");     // stack is empty
+            }
+            else
+            {
+                if (intNumbersStack.Contains(elementToCheck))
+                {
+                    Console.WriteLine("true");
+                }
+                else
+                {
+                    // must print the smallest element
+                    Console.WriteLine(intNumbersStack.Min());
+                }
+            }
+
+        }
+    }
+}
